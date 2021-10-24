b229df51253877f8fe75aeaf5c5cdcee42188e87
DerToaster98 <seiler.oli@web.de>
2021-10-24 11:20:17 +0200

    initial commit

---
+++ src/main/java/de/dertoaster/classichostiles/ClassicHostilesMod.java
@@ -0,0 +1,73 @@
+package de.dertoaster.classichostiles;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import software.bernie.geckolib3.GeckoLib;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ClassicHostilesMod.MODID)
+public class ClassicHostilesMod {
+	
+	public static final String MODID = "classichostiles";
+	
+	// Directly reference a log4j logger.
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+
+	public ClassicHostilesMod() {
+		//Initialize Geckolib
+		GeckoLib.initialize();
+		
+		// Register the setup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		// Register the enqueueIMC method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+		// Register the processIMC method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+		// Register the doClientStuff method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+	}
+
+	private void doClientStuff(final FMLClientSetupEvent event) {
+	}
+
+	private void enqueueIMC(final InterModEnqueueEvent event) {
+	}
+
+	private void processIMC(final InterModProcessEvent event) {
+	}
+
+	// You can use SubscribeEvent and let the Event Bus discover methods to call
+	@SubscribeEvent
+	public void onServerStarting(FMLServerStartingEvent event) {
+	}
+
+	// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+	// Event bus for receiving Registry Events)
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents {
+		@SubscribeEvent
+		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+			// register a new block here
+			LOGGER.info("HELLO from Register Block");
+		}
+	}
+}
---
+++ src/main/java/de/dertoaster/classichostiles/init/CHEntities.java
@@ -0,0 +1,5 @@
+package de.dertoaster.classichostiles.init;
+
+public class CHEntities {
+
+}
